cd41b8c610ed1a395ea5d177cb483ae939353964
Bret Copeland <bret@atlantisflight.org>
2015-09-25 02:41:58 -0400

    Added new parsing tests.

--- test/SchyntaxTestSuite.js
+++ test/SchyntaxTestSuite.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var assert = require('assert');
+var Exceptions = require('../lib/internals/Exceptions');
 var Schedule = require('../lib/Schedule');
 var testsJson = require('./tests.json');
 
@@ -24,15 +25,58 @@ function createTest (info)
 	test(info.format, function ()
 	{
 		var start = new Date(info.date);
-		var prev = new Date(info.prev);
-		var next = new Date(info.next);
+		var prev = info.prev ? new Date(info.prev) : null;
+		var next = info.next ? new Date(info.next) : null;
+		var errorIndex = typeof info.parseErrorIndex === 'number' ? info.parseErrorIndex : null;
 		
-		var sch = new Schedule(info.format);
+		var sch;
 		
-		var actualPrev = sch.previous(start);
-		var actualNext = sch.next(start);
+		try
+		{
+			sch = new Schedule(info.format);
 
-		assert(prev.getTime() === actualPrev.getTime(), 'Previous time was ' + actualPrev.toUTCString() + '. Expected: ' + prev.toUTCString());
-		assert(next.getTime() === actualNext.getTime(), 'Next time was ' + actualNext.toUTCString() + '. Expected: ' + next.toUTCString());
+			if (errorIndex !== null)
+				throw new Error('Expected a parse error at index ' + errorIndex + ', but no error was thrown.');
+		}
+		catch (ex)
+		{
+			if (errorIndex !== null && ex instanceof Exceptions.SchyntaxParseError)
+			{
+				if (ex.index === errorIndex)
+					return;
+				
+				throw new Error('Wrong parse error index. Expected: ' + errorIndex + '. Actual: ' + ex.index);
+			}
+			
+			throw ex;
+		}
+		
+		try
+		{
+			var actualPrev = sch.previous(start);
+			if (prev === null)
+				throw new Error('Expected a ValidTimeNotFoundException. Date returned from previous: ' + actualPrev);
+			
+			assert(prev.getTime() === actualPrev.getTime(), 'Previous time was ' + actualPrev.toUTCString() + '. Expected: ' + prev.toUTCString());
+		}
+		catch (ex)
+		{
+			if (!(ex instanceof Exceptions.ValidTimeNotFoundError) || prev !== null)
+				throw ex;
+		}
+		
+		try
+		{
+			var actualNext = sch.next(start);
+			if (next === null)
+				throw new Error('Expected a ValidTimeNotFoundException. Date returned from next: ' + actualNext);
+			
+			assert(next.getTime() === actualNext.getTime(), 'Next time was ' + actualNext.toUTCString() + '. Expected: ' + next.toUTCString());
+		}
+		catch (ex)
+		{
+			if (!(ex instanceof Exceptions.ValidTimeNotFoundError) || next !== null)
+				throw ex;
+		}
 	});
 }
